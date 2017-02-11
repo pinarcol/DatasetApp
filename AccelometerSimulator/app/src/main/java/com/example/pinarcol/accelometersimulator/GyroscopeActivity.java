01be7127284f5e5737b38c933c5d90473f067349
pinarcol <pinarcol95@gmail.com>
2017-02-11 15:49:21 +0300

    Add gyroscope

---
+++ AccelometerSimulator/app/src/main/java/com/example/pinarcol/accelometersimulator/GyroscopeActivity.java
@@ -0,0 +1,95 @@
+package com.example.pinarcol.accelometersimulator;
+
+/**
+ * Created by pinarcol on 11.2.2017.
+ */
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class GyroscopeActivity extends AppCompatActivity implements SensorEventListener{
+    private SensorManager mSensorManager;
+    private Sensor mGryroscope;
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private final float[] deltaRotationVector = new float[4];
+    private float timestamp;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gyroscope);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mGryroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mGryroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (timestamp != 0) {
+            final float dT = (event.timestamp - timestamp) * NS2S;
+            // Axis of the rotation sample, not normalized yet.
+            float axisX = event.values[0];
+            float axisY = event.values[1];
+            float axisZ = event.values[2];
+
+            // Calculate the angular speed of the sample
+            float omegaMagnitude = (float)Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+            // Normalize the rotation vector if it's big enough to get the axis
+            if (omegaMagnitude > 0.00001) {
+                axisX /= omegaMagnitude;
+                axisY /= omegaMagnitude;
+                axisZ /= omegaMagnitude;
+            }
+
+            // Integrate around this axis with the angular speed by the time step
+            // in order to get a delta rotation from this sample over the time step
+            // We will convert this axis-angle representation of the delta rotation
+            // into a quaternion before turning it into the rotation matrix.
+            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+            float sinThetaOverTwo = (float)Math.sin(thetaOverTwo);
+            float cosThetaOverTwo = (float)Math.cos(thetaOverTwo);
+            deltaRotationVector[0] = sinThetaOverTwo * axisX;
+            deltaRotationVector[1] = sinThetaOverTwo * axisY;
+            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+            deltaRotationVector[3] = cosThetaOverTwo;
+        }
+        timestamp = event.timestamp;
+        float[] deltaRotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+
+        TextView tx=new TextView(this);
+        TextView ty=new TextView(this);
+        TextView tz=new TextView(this);
+
+        tx=(TextView)findViewById(R.id.gyr_x);
+        tx.setText(String.format("Angular acc in x: " + Float.toString(deltaRotationVector[0])));
+
+        ty=(TextView)findViewById(R.id.gyr_y);
+        ty.setText(String.format("Angular acc in y: " + Float.toString(deltaRotationVector[1])));
+
+        tz=(TextView)findViewById(R.id.gyr_z);
+        tz.setText(String.format("Angular acc in z: " + Float.toString(deltaRotationVector[2])));
+        // User code should concatenate the delta rotation we computed with the current rotation
+        // in order to get the updated rotation.
+        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
